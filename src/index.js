f3fe730d84d4efa989f8b2d933be49380fa46f77
Cintra <gui7lk@gmail.com>
2022-11-10 12:13:25 -0300

    some bugs fixeds, and inative participants are deleted

--- src/index.js
+++ src/index.js
@@ -6,27 +6,53 @@ import Joi from 'joi';
 import dayjs from 'dayjs';
 
 dotenv.config();
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-let db, participants, messages;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoClient.connect().then(() => {
-  db = mongoClient.db("bate-papo-uol");
-  participants = db.collection("participants");
-  messages = db.collection("messages");
-});
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+await mongoClient.connect();
+const db = mongoClient.db("bate-papo-uol");
+const participants = db.collection("participants");
+const messages = db.collection("messages");
 
-setInterval(() => {
-  const interval = Date.now() - 10000;
+setInterval(async () => {
+  try {
+    const interval = Date.now() - 10000;
+    const date = dayjs().format('HH:mm:ss');
 
-  participants
-  .deleteMany({lastStatus: {$lt: interval}});
+    const deletedParticipants = await participants
+      .find({ lastStatus: { $lt: interval } })
+      .toArray();
+
+    if (deletedParticipants) {
+      deletedParticipants.forEach(async (participant) => {
+        try {
+          await participants.deleteOne({ id: participant.id });
+
+          messages.insertOne({
+            from: participant.name,
+            to: 'Todos',
+            text: 'Sai da sala...',
+            type: 'status',
+            time: date
+          });
+        } catch (err) {
+
+          console.log(err);
+          res.sendStatus(500);
+        }
+      })
+    }
+  } catch (err) {
+
+    console.log(err);
+    res.sendStatus(500);
+  }
 }, 15000)
 
-app.post('/participants', (req, res, next) => {
+app.post('/participants', async (req, res, next) => {
   const { name } = req.body;
   const date = dayjs().format('HH:mm:ss');
 
@@ -41,46 +67,50 @@ app.post('/participants', (req, res, next) => {
     return res.status(400).send('Preencha todos os campos!');
   }
 
-  participants
-    .findOne({ name: name })
-    .then(user => {
-      if (user) {
-        return res.status(409).send(`O nome ${name} já está em uso!`);
-      } else {
-        participants.insertOne({
-          name,
-          lastStatus: Date.now()
-        });
-        messages.insertOne({
-          from: name,
-          to: 'Todos',
-          text: 'entra na sala...',
-          type: 'status',
-          time: date
-        });
-        return res.sendStatus(201);
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      res.sendStatus(500);
+  try {
+    const user = await participants.findOne({ name: name });
+
+    if (user) {
+      return res.status(409).send(`O nome ${name} já está em uso!`);
+    }
+
+    participants.insertOne({
+      name,
+      lastStatus: Date.now()
     });
+
+    messages.insertOne({
+      from: name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: date
+    });
+
+    return res.sendStatus(201);
+  } catch (err) {
+
+    console.log(err);
+    res.sendStatus(500);
+  }
 });
 
-app.get('/participants', (req, res) => {
-  participants
-    .find()
-    .toArray()
-    .then((participants) => {
-      res.send(participants);
-    });
+app.get('/participants', async (req, res) => {
+  try {
+
+    const activeParticipants = await participants.find().toArray();
+    res.send(activeParticipants);
+  } catch (err) {
+
+    console.log(err);
+    res.sendStatus(500);
+  }
 })
 
-app.post('/messages', (req, res, next) => {
+app.post('/messages', async (req, res, next) => {
   const { to, text, type } = req.body;
   const user = req.headers.user;
   const date = dayjs().format('HH:mm:ss');
-  let contador = 2;
 
   const schema = Joi.object({
     to: Joi.string()
@@ -92,64 +122,69 @@ app.post('/messages', (req, res, next) => {
 
   const result = schema.validate(req.body);
 
-  participants
-    .findOne({ name: user })
-    .then(participant => {
-      if (result.error || !participant) {
-        return res.sendStatus(422);
-      } else {
-        messages.insertOne({
-          from: user,
-          to,
-          text: `mensagem ${contador}`,
-          type,
-          time: date
-        });
-        contador++
-        return res.sendStatus(201);
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      return res.sendStatus(500);
-    });
+  try {
+    const isLogged = await participants.findOne({ name: user });
+
+    if (result.error || !isLogged) {
+      return res.sendStatus(422);
+    } else {
+      messages.insertOne({
+        from: user,
+        to,
+        text,
+        type,
+        time: date
+      });
+    }
+
+    return res.sendStatus(201);
+  } catch (err) {
+
+    console.log(err);
+    return res.sendStatus(500);
+  };
 });
 
-app.get('/messages', (req, res) => {
-  const limit = parseInt(req.query.limit);
-  const user = req.headers.user;
-  
-  if (!limit){
-    messages
-    .find({$or: [{from: user}, {to: "Todos"}, {to: user}]})
-    .toArray()
-    .then((messages) => {
-      return res.send(messages);
-    })
-  }
+app.get('/messages', async (req, res) => {
 
-  messages
-    .find({$or: [{from: user}, {to: "Todos"}, {to: user}]})
-    .sort({$natural:-1})
-    .limit(limit)
-    .toArray()
-    .then((messages) => {
-      return res.send(messages);
-    })
+  try {
+    const limit = parseInt(req.query.limit);
+    const user = req.headers.user;
+
+    if (!limit) {
+      const chatMessages = await messages.find({ $or: [{ from: user }, { to: "Todos" }, { to: user }, { type: "message" }] }).toArray();
+      return res.send(chatMessages);
+    }
+
+    const chatMessages = await messages
+      .find({ $or: [{ from: user }, { to: "Todos" }, { to: user }, { type: "message" }] })
+      .sort({ $natural: -1 })
+      .limit(limit)
+      .toArray();
+
+    return res.send(chatMessages.reverse());
+  } catch (err) {
+
+    console.log(err);
+    res.sendStatus(500);
+  }
 })
 
-app.post('/status', (req, res, next) => {
-  const user = req.headers.user;
-  participants
-  .updateOne({ name: user},{$set :{lastStatus: Date.now()}})
-  .then((updated) => {
-    if(updated.modifiedCount === 1){
+app.post('/status', async (req, res, next) => {
+  try {
+    const user = req.headers.user;
+    const isLogged = await participants.updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+
+    if (isLogged.modifiedCount === 1) {
       return res.sendStatus(200);
     }
 
     return res.sendStatus(404);
-  })
-  .catch(err => console.log(err));
+  } catch (err) {
+
+    console.log(err);
+    res.sendStatus(500);
+  }
 })
 
 app.listen(5000, console.log('Server running in port: 5000'));
